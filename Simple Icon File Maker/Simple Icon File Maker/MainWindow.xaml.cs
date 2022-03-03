fdf3008a26ac8f7f664ab9ce540ea444c96c164e
Joseph Finney <josephfinney@LIVE.COM>
2022-03-02 19:53:25 -0600

    Add project files.

---
+++ Simple Icon File Maker/Simple Icon File Maker/MainWindow.xaml.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Media.Imaging;
+using System;
+using System.Collections.Generic;
+using Windows.ApplicationModel.DataTransfer;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.Storage.Streams;
+using WinRT.Interop;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace Simple_Icon_File_Maker;
+
+/// <summary>
+/// An empty window that can be used on its own or navigated to within a Frame.
+/// </summary>
+public sealed partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private async void OpenBTN_Click(object sender, RoutedEventArgs e)
+    {
+        FileOpenPicker picker = new()
+        {
+            ViewMode = PickerViewMode.Thumbnail,
+            SuggestedStartLocation = PickerLocationId.PicturesLibrary
+        };
+
+        picker.FileTypeFilter.Add(".jpg");
+        picker.FileTypeFilter.Add(".jpeg");
+        picker.FileTypeFilter.Add(".bmp");
+        picker.FileTypeFilter.Add(".png");
+
+        var window = new Microsoft.UI.Xaml.Window();
+        IntPtr hwnd = WindowNative.GetWindowHandle(window);
+        InitializeWithWindow.Initialize(picker, hwnd);
+
+        StorageFile file = await picker.PickSingleFileAsync();
+        if (file is null)
+        {
+            return;
+        }
+
+        // Ensure the stream is disposed once the image is loaded
+        using IRandomAccessStream fileStream = await file.OpenAsync(FileAccessMode.Read);
+        BitmapImage bitmapImage = new();
+        // Decode pixel sizes are optional
+        // It's generally a good optimisation to decode to match the size you'll display
+        // bitmapImage.DecodePixelHeight = decodePixelHeight;
+        // bitmapImage.DecodePixelWidth = decodePixelWidth;
+
+        await bitmapImage.SetSourceAsync(fileStream);
+        MainImage.Source = bitmapImage;
+    }
+
+    private void SaveBTN_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private async void Grid_Drop(object sender, DragEventArgs e)
+    {
+        if (e.DataView.Contains(StandardDataFormats.Bitmap))
+        {
+            e.Handled = true;
+            DragOperationDeferral def = e.GetDeferral();
+            RandomAccessStreamReference s = await e.DataView.GetBitmapAsync();
+            
+            BitmapImage bitmapImage = new();
+            await bitmapImage.SetSourceAsync(await s.OpenReadAsync());
+            MainImage.Source = bitmapImage;
+            e.AcceptedOperation = DataPackageOperation.Copy;
+            def.Complete();
+        } 
+        else if (e.DataView.Contains(StandardDataFormats.Uri))
+        {
+            e.Handled = true;
+            DragOperationDeferral def = e.GetDeferral();
+            Uri s = await e.DataView.GetUriAsync();
+
+            BitmapImage bmp = new(s);
+            MainImage.Source = bmp;
+
+            e.AcceptedOperation = DataPackageOperation.Copy;
+            def.Complete();
+        }
+        else if (e.DataView.Contains(StandardDataFormats.StorageItems))
+        {
+            e.Handled = true;
+
+            DragOperationDeferral def = e.GetDeferral();
+            IReadOnlyList<IStorageItem> storageItems = await e.DataView.GetStorageItemsAsync();
+
+            foreach (var item in storageItems)
+            {
+                if (item is StorageFile file)
+                {
+                    using IRandomAccessStream fileStream = await file.OpenAsync(FileAccessMode.Read);
+                    BitmapImage bitmapImage = new();
+                    // Decode pixel sizes are optional
+                    // It's generally a good optimisation to decode to match the size you'll display
+                    // bitmapImage.DecodePixelHeight = decodePixelHeight;
+                    // bitmapImage.DecodePixelWidth = decodePixelWidth;
+
+                    await bitmapImage.SetSourceAsync(fileStream);
+                    MainImage.Source = bitmapImage;
+
+                    break;
+                }
+            }
+            e.AcceptedOperation = DataPackageOperation.Copy;
+            def.Complete();
+        }
+    }
+
+    private void Border_DragOver(object sender, DragEventArgs e)
+    {
+        e.AcceptedOperation = DataPackageOperation.Copy;
+    }
+}
