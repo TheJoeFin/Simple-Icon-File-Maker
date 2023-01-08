4dbe6d8535e1fabe54525f6387df0877ca89c406
cerealfordinner <cerealfordinnerx@gmail.com>
2023-01-07 22:35:57 -0500

    Added the BItMapExtensions class to handle the try/catch. Current it's still implementing the ContentDialog prompt while I look into getting an InfoBar set up that's bound to the error.

---
+++ Simple Icon File Maker/Simple Icon File Maker/Helpers/BitmapImageExtensions.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media.Imaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage.Streams;
+
+namespace Simple_Icon_File_Maker.Helpers;
+
+public static class BitmapImageExtensions
+{
+    public static async Task CheckAndSetSourceAsync(this BitmapImage bitmapImage, IRandomAccessStream fileStream)
+    {
+    try
+    {
+        await bitmapImage.SetSourceAsync(fileStream);
+    }
+    catch (Exception ex)
+    {
+        ContentDialog dialog = new ContentDialog()
+        {
+            Title = "Error",
+            Content = "An error occurred: " + ex.Message,
+            PrimaryButtonText = "Ok"
+        };
+            dialog.XamlRoot = MainWindow.Current.Content.XamlRoot;
+        await dialog.ShowAsync();
+        return;
+    }
+    }
+}
