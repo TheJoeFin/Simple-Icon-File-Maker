aa3683946409d55bfc57c91f179ce29eeb5790ac
cerealfordinner <cerealfordinnerx@gmail.com>
2023-01-08 00:43:06 -0500

    Started using the ShowErrorInfoBar  method on the MainWindow code behind.

--- Simple Icon File Maker/Simple Icon File Maker/Helpers/BitmapImageExtensions.cs
+++ Simple Icon File Maker/Simple Icon File Maker/Helpers/BitmapImageExtensions.cs
@@ -1,10 +1,5 @@
-﻿using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Media.Imaging;
+﻿using Microsoft.UI.Xaml.Media.Imaging;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
 using System.Threading.Tasks;
 using Windows.Storage.Streams;
 
@@ -12,23 +7,15 @@ namespace Simple_Icon_File_Maker.Helpers;
 
 public static class BitmapImageExtensions
 {
-    public static async Task CheckAndSetSourceAsync(this BitmapImage bitmapImage, IRandomAccessStream fileStream)
+    public static async Task CheckAndSetSourceAsync(this BitmapImage bitmapImage, MainWindow mainWindow, IRandomAccessStream fileStream)
     {
-    try
-    {
-        await bitmapImage.SetSourceAsync(fileStream);
-    }
-    catch (Exception ex)
-    {
-        ContentDialog dialog = new ContentDialog()
+        try
         {
-            Title = "Error",
-            Content = "An error occurred: " + ex.Message,
-            PrimaryButtonText = "Ok"
-        };
-            dialog.XamlRoot = MainWindow.Current.Content.XamlRoot;
-        await dialog.ShowAsync();
-        return;
-    }
+            await bitmapImage.SetSourceAsync(fileStream);
+        }
+        catch (Exception ex)
+        {
+            mainWindow.ShowErrorInfoBar(ex.Message);
+        }
     }
 }
