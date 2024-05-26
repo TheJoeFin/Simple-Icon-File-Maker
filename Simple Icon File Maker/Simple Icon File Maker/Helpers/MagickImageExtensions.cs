0c2d3005c9207c96d49f138d94226c1152491d68
Joseph Finney <TheJoeFin@users.noreply.github.com>
2024-05-26 13:58:33 -0700

    async the stream copy

--- Simple Icon File Maker/Simple Icon File Maker/Helpers/MagickImageExtensions.cs
+++ Simple Icon File Maker/Simple Icon File Maker/Helpers/MagickImageExtensions.cs
@@ -12,7 +12,7 @@ public static class MagickImageExtensions
     public static async Task<ImageSource> ToImageSource(this MagickImage image)
     {
         using MemoryStream memoryStream = new();
-        image.Write(memoryStream, MagickFormat.Png);
+        await image.WriteAsync(memoryStream, MagickFormat.Png);
         memoryStream.Position = 0;
 
         BitmapImage bitmapImage = new();
