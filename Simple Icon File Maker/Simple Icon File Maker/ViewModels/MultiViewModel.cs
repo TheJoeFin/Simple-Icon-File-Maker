abceaa9f204bae0982880a2fbfadc2af5f825b9e
Joseph Finney <josephfinney@live.com>
2024-10-17 16:05:57 -0400

    Got basic multi images loading

--- Simple Icon File Maker/Simple Icon File Maker/ViewModels/MultiViewModel.cs
+++ Simple Icon File Maker/Simple Icon File Maker/ViewModels/MultiViewModel.cs
@@ -5,6 +5,11 @@ using Simple_Icon_File_Maker.Contracts.ViewModels;
 using System.Collections.ObjectModel;
 using Windows.Storage;
 using Simple_Icon_File_Maker.Constants;
+using Microsoft.UI.Xaml;
+using Simple_Icon_File_Maker.Controls;
+using Simple_Icon_File_Maker.Services;
+using Simple_Icon_File_Maker.Models;
+using Simple_Icon_File_Maker.Helpers;
 
 namespace Simple_Icon_File_Maker.ViewModels;
 
@@ -13,6 +18,14 @@ public partial class MultiViewModel : ObservableRecipient, INavigationAware
     private StorageFolder? _folder;
 
     public ObservableCollection<StorageFile> Files { get; } = [];
+
+    public ObservableCollection<UIElement> Previews { get; } = [];
+
+    [ObservableProperty]
+    private int progress = 0;
+
+    [ObservableProperty]
+    private bool loadingImages = false;
 
     [RelayCommand]
     public void GoBack()
@@ -39,20 +52,42 @@ public partial class MultiViewModel : ObservableRecipient, INavigationAware
         if (parameter is StorageFolder folder)
             _folder = folder;
 
-        await LoadFiles();
+        Progress<int> progress = new(percent =>
+        {
+            Progress = percent;
+        });
+
+        await LoadFiles(progress);
     }
 
-    private async Task LoadFiles()
+    private async Task LoadFiles(IProgress<int> progress)
     {
         if (_folder is null)
             return;
 
+        LoadingImages = true;
         Files.Clear();
 
         IReadOnlyList<StorageFile> tempFiles = await _folder.GetFilesAsync();
 
+        List<IconSize> sizes = [..IconSize.GetWindowsSizesFull()];
+
         foreach (StorageFile file in tempFiles)
-            if (file.IsSupportedImageFormat())
-                Files.Add(file);
+        {
+            if (!file.IsSupportedImageFormat())
+                continue;
+
+            PreviewStack preview = new(file.Path, sizes)
+            {
+                MaxWidth = 600,
+                MinWidth = 200,
+                Margin = new Thickness(6)
+            };
+
+            Previews.Add(preview);
+            bool successs = await preview.InitializeAsync(progress);
+        }
+
+        LoadingImages = false;
     }
 }
