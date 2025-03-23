76996969a811c97ca93bf974b49081655aed9a89
Joseph Finney <josephfinney@LIVE.COM>
2025-03-22 19:34:19 -0500

    Initial add WCR and update refs

---
+++ Simple Icon File Maker/Simple Icon File Maker/Helpers/WcrHelper.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Graphics.Imaging;
+using Microsoft.Windows.Management.Deployment;
+using Windows.Graphics;
+using Windows.Graphics.Imaging;
+
+namespace Simple_Icon_File_Maker.Helpers;
+
+public class WcrHelper
+{
+    public async Task<SoftwareBitmap> ExtractImage(SoftwareBitmap softwareBitmap, PointInt32[] selectionPoints)
+    {
+        if (!ImageObjectExtractor.IsAvailable())
+        {
+            var result = await ImageObjectExtractor.MakeAvailableAsync();
+            if (result.Status != PackageDeploymentStatus.CompletedSuccess)
+            {
+                throw result.ExtendedError;
+            }
+        }
+
+        ImageObjectExtractor imageObjectExtractor = await ImageObjectExtractor.CreateWithSoftwareBitmapAsync(softwareBitmap);
+
+        ImageObjectExtractorHint hint = new(
+                        includeRects: null,
+                        includePoints: selectionPoints,
+                        excludePoints: null
+                    );
+
+        SoftwareBitmap finalImage = imageObjectExtractor.GetSoftwareBitmapObjectMask(hint);
+
+        return finalImage;
+    }
+}
