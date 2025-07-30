5c03c39bd70d658e1bb9a754ed239c3093c70ecc
Joseph Finney <josephfinney@live.com>
2025-07-29 19:20:03 -0500

    Move to Titlebar from WinUI and remove unused code

--- Simple Icon File Maker/Simple Icon File Maker/Views/ShellPage.xaml.cs
+++ Simple Icon File Maker/Simple Icon File Maker/Views/ShellPage.xaml.cs
@@ -1,5 +1,5 @@
-using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Simple_Icon_File_Maker.Contracts.Services;
 using Simple_Icon_File_Maker.ViewModels;
 
 namespace Simple_Icon_File_Maker.Views;
@@ -20,11 +20,9 @@ public sealed partial class ShellPage : Page
 
         App.MainWindow.ExtendsContentIntoTitleBar = true;
         App.MainWindow.SetTitleBar(titleBar);
-        App.MainWindow.Activated += MainWindow_Activated;
-    }
-
-    private void MainWindow_Activated(object sender, WindowActivatedEventArgs args)
-    {
         App.AppTitlebar = titleBar;
+
+        if (App.GetService<IStoreService>().OwnsPro)
+            titleBar.Subtitle += " Pro";
     }
 }
