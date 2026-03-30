1a00eaeaeb90e0d4820f70e49f6e26de05c46446
Joe Finney <josephfinney@LIVE.COM>
2026-03-29 21:05:40 -0500

    Add theme switcher & rating/feedback to About page
    Update About page with theme selection (light/dark/default) and in-app rating control. Show changelog for v1.16/v1.15. Add feedback dialog for low ratings and Store review for high ratings. Escape key now navigates back. Bump app version to 1.16.0.0. Minor refactoring and new dependencies in AboutViewModel.

--- Simple Icon File Maker/Simple Icon File Maker/ViewModels/AboutViewModel.cs
+++ Simple Icon File Maker/Simple Icon File Maker/ViewModels/AboutViewModel.cs
@@ -1,11 +1,16 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using Microsoft.UI.Xaml;
 using Simple_Icon_File_Maker.Contracts.Services;
 
 namespace Simple_Icon_File_Maker.ViewModels;
 
 public partial class AboutViewModel : ObservableRecipient
 {
+    private readonly IThemeSelectorService _themeSelectorService;
+
+    public ElementTheme Theme => _themeSelectorService.Theme;
+
     [RelayCommand]
     public void GoBack()
     {
@@ -17,8 +22,15 @@ public partial class AboutViewModel : ObservableRecipient
         get;
     }
 
-    public AboutViewModel(INavigationService navigationService)
+    public AboutViewModel(INavigationService navigationService, IThemeSelectorService themeSelectorService)
     {
         NavigationService = navigationService;
+        _themeSelectorService = themeSelectorService;
+    }
+
+    public async Task SwitchThemeAsync(ElementTheme theme)
+    {
+        await _themeSelectorService.SetThemeAsync(theme);
+        OnPropertyChanged(nameof(Theme));
     }
 }
